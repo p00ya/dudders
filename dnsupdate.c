p00ya/dudders

commit c3b622e04a0eee9c6e9084319d9b4c3c943b7b07

Check DNS UPDATE response size
Be a little more informative when abnormally short DNS UPDATE
responses are received.

dnsupdate.c
@@ -250,6 +250,8 @@ enum {
 void
 check_dnsupdate_response(const unsigned char *response, size_t length)
 {
+	hope(NS_HFIXEDSZ < length,
+	    "server returned incomplete DNS UPDATE response");
 	uint16_t flags = ns_get16(response + response_flags);
 	hope(0xa800 == (flags & 0xff00),
 	    "possible spurious DNS UPDATE response");

commit b7fc294bf5e91a25cfe5de1b8d06095aeba640f3

Fix header size check
Correct the off-by-1 error introduced in c3b622e0, causing valid
responses to be interpreted as failures.

dnsupdate.c
@@ -250,7 +250,7 @@ enum {
 void
 check_dnsupdate_response(const unsigned char *response, size_t length)
 {
-	hope(NS_HFIXEDSZ < length,
+	hope(NS_HFIXEDSZ <= length,
 	    "server returned incomplete DNS UPDATE response");
 	uint16_t flags = ns_get16(response + response_flags);
 	hope(0xa800 == (flags & 0xff00),