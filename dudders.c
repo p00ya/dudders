7c02bb7088d899e57934b7ab03de4dc9d6b85cd3
Dean Scarff <dos@scarff.id.au>
2020-12-02 19:56:47 +1100

    Remove copyright boilerplate
    Remove year from copyright notice (so it doesn't need to be bumped) and
    disclaimer notice from each file.  Add the full notice to the README.

--- dudders.c
+++ dudders.c
@@ -1,18 +1,9 @@
 /* dudders.c -- Use SIG(0) key to renew a dynamic DNS domain
  *
- * Copyright 2007--2010 Dean Scarff
+ * Copyright Dean Scarff
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
- * may not use this file except in compliance with the License.  You
- * may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * may not use this file except in compliance with the License.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -75,7 +66,7 @@ usage()
 static void
 version()
 {
-	printf("%s\n\n%s\n", PACKAGE_STRING, "Copyright 2007 Dean Scarff");
+	printf("%s\n\n%s\n", PACKAGE_STRING, "Copyright Dean Scarff");
 }
 
 static const char *key_filename; // filename for inference
